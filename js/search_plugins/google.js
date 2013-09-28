2eec0042d8e873a9ffe9c669b56c05754b4a3b8f
Enno Gottschalk <mrmaffen@googlemail.com>
2013-09-28 17:09:08 +0200

    Merged google.js

--- js/search_plugins/google.js
+++ js/search_plugins/google.js
@@ -1,54 +1,51 @@
-/**
- * The google-API allows a maximum of 64 images,
- *	spread over 8 pages, to be returned.
- *
- *	URL-Parameters:
- *	"&rsz=8"
- *		8 images per page (maximum value)
- *
- *	"&start=img"
- *		Each resultset can only contain the images
- *		of one page. The start-Parameter is an offset to access
- *		sites 2, 3 etc.
- */
+//	The google-API allows a maximum of 64 images,
+//	spread over 8 pages, to be returned.
+//
+//	URL-Parameters:
+//	"&rsz=8"
+//		8 images per page (maximum value)
+//
+//	"&start=img"
+//		Each resultset can only contain the images
+//		of one page. The start-Parameter is an offset to access
+//		sites 2, 3 etc.
 
-function searchGoogle(keyWord) {
-	var resultArray = new Array();
+// Loop through the 8 pages
 
-	// google API doesnt automatically normalize searchterms
-	keyWord = keyWord.replace(' ', '%20');
-	if( !(typeof(keyWord)=="undefined")){
+function searchGoogle(keyWord, stepSize, offset) {
 
-		// Loop through the 8 pages
-		for (var img=0; img<64; img+=8){
-			$.ajax({
-				url: "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + keyWord + "&rsz=8&start="+img,
-				dataType: "jsonp",
-				success: function(data) {
-					for (var i = 0; i < data.responseData.results.length; i++) {
-						var currentImg = data.responseData.results[i];
-						resultArray.push({
+    this.name = "Google";
+    
+    // google API doesnt automatically normalize searchterms    
+    keyWord = keyWord.replace(' ', '%20');
+    var promise = RSVP.Promise(function(resolve, reject){
+        $.ajax({
+            url: "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + keyWord + "&rsz=" + stepSize + "&start=" + offset,
+            dataType: "jsonp"
+        }).then(function(data) {
+                if (typeof(keyWord)=="undefined" || !data.responseData){
+                    reject("Google responded: '"+data.responseDetails+"'");
+                } else {
+                    var a = [];
+                    for (var i = 0; i < data.responseData.results.length; i++) {
+                        var currentImg = data.responseData.results[i];
+                        a.push({
+                            //Elemente des Suchergebnisses ins Array pushen
 
-							//Anzeigename des Bilds
-							title: currentImg.titleNoFormatting,
-							//Thumbnail URL
-							tnUrl: currentImg.tbUrl,
-							//DirectLink
-							url: currentImg.url,
-							//URL mit der Seite des Bilds
-							siteUrl: currentImg.originalContextUrl
-						});
-					}
-				}
-			});
-		}
-	}
-
-	//	Wait for the 8 queries to finish,
-	//	then return a beautiful JSON :)
-	$(document).ajaxStop(function () {
-		console.log("google:");
-		console.dir(resultArray);
-		return JSON.stringify(resultArray);
-	});
+                            //Anzeigename des Bilds
+                            title: currentImg.titleNoFormatting,
+                            //Thumbnail URL
+                            tnUrl: currentImg.tbUrl,
+                            //DirectLink
+                            url: currentImg.url,
+                            //URL mit der Seite des Bilds
+                            siteUrl: currentImg.originalContextUrl
+                        });
+                    }
+                    resolve(a);
+                }
+            }, reject);
+    });
+    
+    return promise;
 }
