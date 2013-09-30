13f9181022378a790659f8bcbf8ab650c85300c3
Enno Gottschalk <mrmaffen@googlemail.com>
2013-09-30 20:38:13 +0200

    Don't query twice.

--- js/router.js
+++ js/router.js
@@ -17,25 +17,30 @@ Imagical.ImagicalRoute = Ember.Route.extend({
 Imagical.TermRoute = Ember.Route.extend({
     setupController: function(controller, model){
         var that = this;
-        for (var i=0;i<searchPlugins.length;i++){
-          if (searchPlugins[i].isEnabled) {
-            console.log('Querying "'+searchPlugins[i].pluginName+'" for "' + model.get('termText') + '"');
-            searchPlugins[i].pluginFunction(model.get('termText')).then(function(data) {
-                console.dir(data);
-                for (var i = 0; i < data.length; i++ ){
-                    var imageResult = that.store.createRecord('imageresult', {
-                        siteUrl: data[i].siteUrl,
-                        title: data[i].title,
-                        tnUrl: data[i].tnUrl,
-                        url: data[i].url
-                    });
-                    imageResult.get('terms').pushObject(model);
-                    imageResult.save();
-                }
-            }).then(null, function(reason){
-                console.error(reason);
-            });
-          }
+        if (!model.get('imageresults'))
+            model.set('hasBeenQueried', false);
+        if (!model.get('hasBeenQueried')){
+            model.set('hasBeenQueried', true);
+            for (var i=0;i<searchPlugins.length;i++){
+              if (searchPlugins[i].isEnabled) {
+                console.log('Querying "'+searchPlugins[i].pluginName+'" for "' + model.get('termText') + '"');
+                searchPlugins[i].pluginFunction(model.get('termText')).then(function(data) {
+                    console.dir(data);
+                    for (var i = 0; i < data.length; i++ ){
+                        var imageResult = that.store.createRecord('imageresult', {
+                            siteUrl: data[i].siteUrl,
+                            title: data[i].title,
+                            tnUrl: data[i].tnUrl,
+                            url: data[i].url
+                        });
+                        imageResult.get('terms').pushObject(model);
+                        imageResult.save();
+                    }
+                }).then(null, function(reason){
+                    console.error(reason);
+                });
+              }
+            }
         }
         
         controller.set('model', model);
